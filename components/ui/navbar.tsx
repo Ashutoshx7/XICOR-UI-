637075d8e8cef0cd3d9dfae6ffde35bed6e098e4
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-19 18:05:27 +0530

    login

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-center  '>
-      <div className='w-full h-10  flex justify-center bg-black  text-red-500 font-sans border-b-3 border-blue-500 items-center gap-3 '>Features 
-        <button className=' bg-blue-800 w-15 h-6 rounded-2xl  text-sm py-0.5 inner flex justify-center text-white font-sans inset-shadow-sm inset-shadow-indigo-500/50'>Login</button>Pricing contact
+      <div className='w-full h-10  flex justify-center bg-black  text-white font-sans border-b-3 border-blue-500 items-center gap-3 '>Features 
+        <button className=' bg-blue-800 w-15 h-6 rounded-2xl  text-sm py-0.5 inner flex justify-center text-white font-sans inset-shadow-sm  inset-shadow-blue-500 border-accent-foreground '>Login</button>Pricing contact
       </div>
 
     </div>
