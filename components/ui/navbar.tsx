cd3ea7757b475cabe8b4b137c742113824fe9bab
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-19 15:53:38 +0530

    repo-structure/navbar

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-center '>
+      <div className='w-full flex justify-center bg-black font-bold text-red-300 font-mono border-b-2 border-white '>Navbar</div>
+    </div>
+  );
+}
+
+export default Navbar
