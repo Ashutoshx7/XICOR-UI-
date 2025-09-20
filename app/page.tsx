240b2f1ba4e3be540699400bc8c37695934a9f84
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-21 02:34:49 +0530

    docs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import  Navbar  from "@/components/ui/navbar";
-import Glassdock from "@/components/ui/glassdock";
+import GlassdockCore from "@/components/ui/glassdock";
 export default function Home() {
   return (
     <div>
       
-     <Glassdock></Glassdock>
+     <GlassdockCore/>
     </div>
     
   );
--- components/ui/glassdock.tsx
+++ components/ui/glassdock.tsx
@@ -2,68 +2,65 @@
 
 import Link from "next/link";
 import {
-  IconBrandGithub,
-  IconBrandX,
-  IconExchange,
-  IconHome,
-  IconNewSection,
-  IconTerminal,
-  IconTerminal2,
+    IconBrandGithub,
+    IconBrandX,
+    IconExchange,
+    IconHome,
+    IconNewSection,
+    IconTerminal,
+    IconTerminal2,
 } from "@tabler/icons-react";
 import React from "react";
 
 const GlassdockCore = () => {
-  const links = [
-    {
-      title: "Home",
-      icon: <IconHome className="h-full w-full text-neutral-400" />,
-      href: "/",
-    },
-    {
-      title: "Products",
-      icon: <IconTerminal2 className="h-full w-full text-neutral-400" />,
-      href: "/product",
-    },
-    {
-      title: "Components",
-      icon: <IconNewSection className="h-full w-full text-neutral-400" />,
-      href: "/components",
-    },
-    {
-      title: "Archive",
-      icon: <IconTerminal className="h-full w-full text-neutral-400" />,
-      href: "/archive",
-    },
-    {
-      title: "ChangeLog",
-      icon: <IconExchange className="h-full w-full text-neutral-400" />,
-      href: "/changelog",
-    },
-    {
-      title: "Twitter",
-      icon: <IconBrandX className="h-full w-full text-neutral-400" />,
-      href: "https://twitter.com",
-    },
-    {
-      title: "Github",
-      icon: <IconBrandGithub className="h-full w-full text-neutral-400" />,
-      href: "https://github.com",
-    },
-  ];
+    const links = [
+        { title: "Home", icon: IconHome, href: "/" },
+        { title: "Products", icon: IconTerminal2, href: "/product" },
+        { title: "Components", icon: IconNewSection, href: "/components" },
+        { title: "Archive", icon: IconTerminal, href: "/archive" },
+        { title: "ChangeLog", icon: IconExchange, href: "/changelog" },
+        { title: "Twitter", icon: IconBrandX, href: "https://twitter.com" },
+        { title: "Github", icon: IconBrandGithub, href: "https://github.com" },
+    ];
 
-  return (
-    <div className="fixed bottom-6 inset-x-0 mx-auto flex justify-center space-x-6 bg-white/10 backdrop-blur-[2px]  rounded-2xl border border-white/10  before:content-[''] before:absolute before:inset-0-before:rounded-2xl before:border before:border-white/20  py-3 px-6  w-fit">
-      {links.map((el) => (
-        <Link
-          key={el.title}
-          href={el.href}
-          className="flex items-center justify-center w-10 h-"
-        >
-          {el.icon}
-        </Link>
-      ))}
-    </div>
-  );
+    return (
+    
+        <div
+            className="
+        fixed bottom-6 inset-x-0 mx-auto 
+        flex justify-center space-x-8
+        rounded-3xl border border-black-500/50 
+        blur-in-xl  backdrop-blur-lg
+        py-3 px-6 w-fit  inset-shadow-sm inset-shadow-slate-500/50
+      
+        
+       
+         
+
+      "
+      
+            
+        > 
+            {links.map((el) => {
+                const Icon = el.icon;
+                return (
+                    <Link
+                        key={el.title}
+                        href={el.href}
+                        className="
+              flex items-center justify-center w-10 h-12
+               text-gray-900 hover:text-gray-700
+              
+            "
+                    >
+                        <Icon className="w-full h-full" />
+                        
+                    </Link>
+                );
+            })}
+        </div>
+       
+    );
 };
 
 export default GlassdockCore;
