9851f87d773c49cdce06bc0448e93c58b08e58ef
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-27 20:06:30 +0530

    bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import  Navbar  from "@/components/ui/navbar";
 import GlassdockCore from "@/components/ui/glassdock";
+import Bentogird from "@/components/ui/bentogird";
+
 export default function Home() {
   return (
     <div>
-      
-     <GlassdockCore/>
-    </div>
+      <Bentogird/>
+      </div>
     
   );
 }
---
+++ components/ui/bentogird.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bentogird = () => {
+  return (
+    <div>
+        Hello world
+      
+    </div>
+  )
+}
+
+export default Bentogird
