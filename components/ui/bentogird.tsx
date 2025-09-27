1dc30d8450163fba329cb5ea2ab495517c9198a5
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-27 21:43:44 +0530

    layout

--- components/ui/bentogird.tsx
+++ components/ui/bentogird.tsx
@@ -3,13 +3,11 @@ import React from 'react'
 const Bentogird = () => {
   return (
     <div className='max-w-7xl  border-neutral-200 bg-gray-100 mx-auto border-x min-h-screen'>
-        <div className='grid grid-cols-6 '>
-            <div className='h-40 w-40 bg-red-400'></div>
-            <div className='h-40 w-40 bg-purple-300'></div>
-            <div className='h-40 w-40 bg-yellow-400'></div>
-            <div className='h-40 w-40 bg-orange-400'></div>
-            <div className='h-40 w-40 bg-blue-400'></div>
-            <div className='h-40 w-40 bg-green-400'></div>
+        <div className='grid grid-cols-3 gap-10'>
+            <div className='h-40 w-full bg-red-400 col-span-1 '></div>
+            <div className='h-40 w-full bg-purple-300  col-span-2'></div>
+            <div className='h-40 w-full bg-yellow-400 col-span-2'></div>
+            <div className='h-40 w-full bg-orange-400 col-span-1'></div>
         </div>
         Hello world
       
